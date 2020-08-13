3840b57cf348a6631af7bf11238348af94104701
Eihab <mastereihab@gmail.com>
2020-08-13 15:20:47 +0100

    Added score display animation

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 const correctAnswers = ["B","A","B","A"];
 const form = document.querySelector(".quiz-form")
 const resultContainer = document.querySelector(".result")
-const scoreSpan = resultContainer.querySelector("span");
+
 
 function updateScore(score) {
-    resultContainer.classList.remove("d-none")
-    scoreSpan.textContent = `${score}%`
+    
+    
 }
 
 form.addEventListener("submit", e => {
     e.preventDefault();
 
     let score = 0;
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q3.value]
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value]
+    console.log(userAnswers)
     userAnswers.forEach((answer, index) => {
         if (answer === correctAnswers[index]) {
             score += 25;
         }
     });
-    console.log(scoreSpan.textContent)
-    updateScore(score);
     scrollTo(0,0)
+    resultContainer.classList.remove("d-none")
+
+    // Animate Result
+    let output = 0;
+    const timer = setInterval(()=> {
+        resultContainer.querySelector("span").textContent = `${output}%`;
+        if (output === score) {
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 10);
+
 });
 
